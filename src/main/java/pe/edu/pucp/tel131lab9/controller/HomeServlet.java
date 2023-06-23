0bf3f4da18ca5d1732042edc919923f8d7baa56b
Willy Huallpa <willy.huallpa@pucp.edu.pe>
2023-06-22 21:38:35 -0500

    Buscador implementado

--- src/main/java/pe/edu/pucp/tel131lab9/controller/HomeServlet.java
+++ src/main/java/pe/edu/pucp/tel131lab9/controller/HomeServlet.java
@@ -6,9 +6,12 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import pe.edu.pucp.tel131lab9.dao.EmployeeDao;
 import pe.edu.pucp.tel131lab9.dao.PostDao;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.SQLException;
 
 @WebServlet(name = "HomeServlet", urlPatterns = {"/HomeServlet",""})
 public class HomeServlet extends HttpServlet {
@@ -23,4 +26,25 @@ public class HomeServlet extends HttpServlet {
         view = request.getRequestDispatcher("home.jsp");
         view.forward(request, response);
     }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        String action = request.getParameter("action") == null ? "guardar" : request.getParameter("action");
+        PostDao postDao = new PostDao();
+
+        switch(action) {
+                case "buscar":
+                String textoBuscar = request.getParameter("textoBuscar");
+                if (textoBuscar == null) {
+                    response.sendRedirect("Homervlet");
+                } else {
+                    request.setAttribute("textoBusqueda", textoBuscar);
+                    System.out.println("sevlet" + postDao.findPostByName(textoBuscar).size());
+                    request.setAttribute("posts", postDao.findPostByName(textoBuscar));
+                    RequestDispatcher view = request.getRequestDispatcher("home.jsp");
+                    view.forward(request, response);
+                }
+                break;
+            }
+    }
+
 }
--- src/main/webapp/home.jsp
+++ src/main/webapp/home.jsp
@@ -2,6 +2,9 @@
 <%@ page import="pe.edu.pucp.tel131lab9.bean.Post" %>
 <jsp:useBean id="posts" type="java.util.ArrayList<pe.edu.pucp.tel131lab9.bean.Post>" scope="request"/>
 <%@page contentType="text/html" pageEncoding="UTF-8" %>
+<jsp:useBean id="textoBusqueda" scope="request" type="java.lang.String" class="java.lang.String"/>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -22,6 +25,20 @@
             <a href="<%= request.getContextPath()%>/PostServlet?action=new" class="btn btn-primary">New Post</a>
         </div>
     </div>
+
+    <form method="post" action="<%=request.getContextPath()%>/HomeServlet?action=buscar">
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Buscar " name="textoBuscar"
+                   value="<%=textoBusqueda%>"/>
+            <button class="input-group-text" type="submit">
+                <i class="bi bi-search"></i>
+            </button>
+            <a class="input-group-text" href="<%=request.getContextPath()%>/HomeServlet">
+                <i class="bi bi-x-circle"></i>
+            </a>
+        </div>
+    </form>
+
     <div class="row">
         <%for (Post p : posts) {%>
         <div class="col-sm-4 py-3">
